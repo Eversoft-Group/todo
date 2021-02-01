54b66536d3071416afe05b698ba59023bd393a65
Saroj Tripathi <sar.vhanta@gmail.com>
2021-02-01 17:20:34 +0100

    Solved the user field required option

--- todoapp/task/views.py
+++ todoapp/task/views.py
@@ -1,10 +1,8 @@
 from rest_framework import generics
 from .serializers import TaskSerializer
 from .models import Task
-from rest_framework import permissions
-
-# Create your views here.
-
+from rest_framework import permissions, status
+from rest_framework.response import Response
 
 class TaskListView(generics.ListCreateAPIView):
     serializer_class = TaskSerializer
@@ -12,6 +10,14 @@ class TaskListView(generics.ListCreateAPIView):
 
     def get_queryset(self):
         return Task.objects.filter(user=self.request.user)
+
+    def post(self, request, *args, **kwargs):
+        serializer=TaskSerializer(data=request.data, context={'request': request})
+        if serializer.is_valid():
+            serializer.save()
+        else:
+            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+        return Response({"detail": serializer.data}, status=status.HTTP_201_CREATED)
 
 class TaskDetailView(generics.RetrieveUpdateDestroyAPIView):
     serializer_class = TaskSerializer
